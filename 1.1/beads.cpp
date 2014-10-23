245c90e631ec3a54f66c2416bc8014a7fda68751
wyuan <wyuan1704@gmail.com>
2014-10-23 08:44:18 +0800

    Rewrite an easier solution in the official analisis for beads.cpp

--- 1.1/beads.cpp
+++ 1.1/beads.cpp
@@ -3,7 +3,49 @@
  PROG: beads
  LANG: C++
  */
+//There is a very easier solution in the official Analisis. I like "state". Rewrite it here.
+#include <fstream>
+#include <assert.h>
+using namespace std;
 
+
+int main(){
+    ifstream fin("beads.in");
+    int N;
+    string beads;
+    fin>> N >> beads;
+    assert(N == beads.size());
+    fin.close();
+    
+    beads += beads;
+    int max = 0;
+    int state; // 0 -> only 'w'; 1 -> 'w' + ('b' or 'r'); 2 -> all
+    for(int i=0; i<N; i++)
+    {
+        char cur = beads[i];
+        if(cur == 'w')
+            state = 0;
+        else
+            state = 1;
+        int end = i;
+        while(state < 3)
+        {
+            while(end < N+i && (beads[end] == cur || beads[end] == 'w'))
+                end++;
+            state++;
+            cur = beads[end];
+        }
+        if(end - i > max)
+            max = end - i;
+    }
+    ofstream fout("beads.out");
+    fout<< max;
+    fout.close();
+    return 0;
+}
+
+
+/*
 // wyuan on 10/23/14. From one breaking point, we should count two ways. When we get one end, we should cross a virtual line to the other end, which is a little hard to code. Let's connect two copies of the beads, then we don't have to go outside. If the count is larger than the origial size, just print the original size.
 
 #include <fstream>
@@ -65,4 +107,4 @@ int main(){
     
     return 0;
 }
-
+*/

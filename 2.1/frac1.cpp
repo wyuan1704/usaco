7e6d31b450dffb312b2e9c05da69752ec88130ef
wyuan <wyuan1704@gmail.com>
2014-10-26 10:45:10 +0800

    wyuann; add frac1; Generate all fractions and sort them.

---
+++ 2.1/frac1.cpp
@@ -0,0 +1,65 @@
+/*
+ID: wyuan171
+PROG: frac1
+LANG: C++
+*/
+
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+struct Fraction
+{
+    int n; //numerator
+    int d; //Denominator;
+    Fraction(int in, int id):n(in), d(id){};
+};
+
+int comp(const Fraction &a, const Fraction &b)
+{
+    return a.n * b.d < a.d * b.n;
+}
+
+class Solution
+{
+
+public:
+	Solution(const char *finName, const char*foutName): fin(finName), fout(foutName){};
+	void main()
+	{
+        size_t N;
+        fin >> N;
+        //N = 5;
+        vector<Fraction> fracs;
+        fracs.push_back(Fraction(0, 1));
+        for(int i=1; i<N; i++)
+            for(int j=i+1; j<=N; j++)
+            {
+                if(i == 1 || j%i != 0)
+                    fracs.push_back(Fraction(i, j));
+            }
+		
+        sort(fracs.begin(), fracs.end(), comp);
+        fracs.push_back(Fraction(1, 1));
+        
+        size_t size = fracs.size();
+        for(int i=0; i < size; i++)
+            fout << fracs[i].n << '/' << fracs[i].d << endl;
+        
+		fout<<endl;
+		fin.close();
+		fout.close();
+	}
+private:
+	ifstream fin;
+	ofstream fout;
+};
+
+int main()
+{
+	Solution sol("frac1.in", "frac1.out");
+	sol.main();
+	return 0;
+}

9428dcba0d92ba0fd41300985166e56e9f8a340e
wyuan <wyuan1704@gmail.com>
2014-10-27 22:13:13 +0800

    wyuan; money; DFS -  Time exceeded for #8 - same with combination-sum on leetcode

---
+++ 2.3/money.cpp
@@ -0,0 +1,73 @@
+/*
+ID: wyuan171
+PROG: money
+LANG: C++
+*/
+
+#include <fstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// 10/27/2014; https://oj.leetcode.com/problems/combination-sum/
+
+class Solution
+{
+    unsigned long long combinationSum(vector<int> &candidates, const int &target) {
+        
+        sort(candidates.begin(), candidates.end());
+        unsigned long long res=0;
+    
+        helper(res, candidates, 0, 0, target);
+        return res;
+    }
+    void helper(unsigned long long &res, vector<int> &candidates, int curSum, int start, const int &target)
+    {
+        for(int i = start; i<V; i++)
+        {
+            int cur = candidates[i] + curSum;
+            if(cur > target)
+                return;
+            else if(cur == target)
+            {
+                res++;
+                return;
+            }
+            else
+            {
+                // The same repeated number may be chosen from C unlimited number of times.!!!
+                //helper(res, candidates, curRes, curSum + candidates[i], i+1, target);
+                helper(res, candidates, cur, i, target);
+            }
+        }
+    }
+    
+public:
+	Solution(const char *finName, const char*foutName): fin(finName), fout(foutName){};
+	void main()
+	{
+        fin >> V;
+        int N;
+        fin >> N;
+        vector<int> coins(V);
+        for(int i=0; i<V; i++)
+            fin >> coins[i];
+        
+        fout << combinationSum(coins, N);
+		
+		fout<<endl;
+		fin.close();
+		fout.close();
+	}
+private:
+	ifstream fin;
+	ofstream fout;
+    int V;
+};
+
+int main()
+{
+	Solution sol("money.in", "money.out");
+	sol.main();
+	return 0;
+}

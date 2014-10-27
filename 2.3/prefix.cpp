24ddf4df0afe8cde800bb59b2fe77254adeffe35
wyuan <wyuan1704@gmail.com>
2014-10-27 21:05:29 +0800

    wyuan; prefix; AC - optimize longestPrefix - forward seting DP than checking backward.

--- 2.3/prefix.cpp
+++ 2.3/prefix.cpp
@@ -10,39 +10,31 @@ LANG: C++11
 #include <vector>
 using namespace std;
 
-// 10/27/2014; This is like https://oj.leetcode.com/problems/word-break/
+// 10/27/2014; This is like https://oj.leetcode.com/problems/word-break/. Leetcode is loose. USACO is more strict. In fact, there're solutions much faster than this. To do - optimize.
 
 class Solution
 {
-    int longestPrefix(string s, unordered_set<string> &dict) {
+    int longestPrefix(string &s, unordered_set<string> &dict, int maxWordLen) {
         // wyuan; 10/09/2014; Dynamic programming. dp[i] means that s[0]~s[i] can be segmented.
         int size = s.size();
-        vector<bool> dp(size, false);
-        for(int i = 0; i < size; i++)
+        vector<bool> dp(size+1, false);
+        dp[0] = true;
+        for(int i = 0; i <= size; i++)
         {
-            if(dp[i])
-                continue;
-            // if 0~i can be found in dict
-            dp[i] = (dict.find(s.substr(0, i+1)) != dict.end() ? true:false);
             if(!dp[i])
+                continue;
+            
+            for(int len = 1; len <= maxWordLen; len++)
             {
-                // Check if it can be formed be smaller words
-                for(int j=0; j<i; j++)
-                {
-                    if(dp[j])
-                    {
-                        dp[i] = (dict.find(s.substr(j+1, i-j)) != dict.end() ? true:false);
-                        if(dp[i])
-                            break;
-                    }
-                }
+                if(!dp[i+len] && dict.find(s.substr(i, len)) != dict.end())
+                   dp[i+len] = true;
             }
         }
         
-        for(int i=size-1; i>=0; i--)
+        for(int i=size; i>0; i--)
         {
             if(dp[i])
-                return i+1;
+                return i;
         }
         
         return 0;
@@ -52,11 +44,13 @@ public:
 	void main()
 	{
         unordered_set<string> dict;
-        
+        int maxWordLen = 0;
         string word("");
         fin >> word;
         while(word != ".")
         {
+            if(maxWordLen < word.size())
+                maxWordLen = word.size();
             dict.insert(word);
             fin >> word;
         }
@@ -69,7 +63,7 @@ public:
             fin >> word;
         }
         
-        fout << longestPrefix(target, dict);
+        fout << longestPrefix(target, dict, maxWordLen);
         
 		fout<<endl;
 		fin.close();
